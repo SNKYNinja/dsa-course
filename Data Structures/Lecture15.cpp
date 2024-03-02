63a14fb33bdbedc2908ecedabdc39639dab7d977
Aditya Chaudhary <96983394+SNKYNinja@users.noreply.github.com>
2024-03-02 21:24:41 +0530

    feat: Add lecture15 code

---
+++ Data Structures/Lecture15.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 1. Book Allocation Problem ********************
+    // https://www.codingninjas.com/studio/problems/allocate-books_1090540
+
+    // 2. Painter's Partition Problem ********************
+    // https://www.codingninjas.com/studio/problems/painter-s-partition-problem_1089557
+
+    // 3. Agressive Cows Problem ***********************
+    // https://www.codingninjas.com/studio/problems/aggressive-cows_1082559
+
+    
+}
